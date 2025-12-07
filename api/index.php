320ecf5151944d94baa250a632f99af30029f293
Kazeth <roonysoon88@gmail.com>
2025-12-07 17:12:15 +0700

    Update : Adjust project's stucture for deployment

--- api/index.php
+++ api/index.php
@@ -1,11 +1,2 @@
 <?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-$app = require __DIR__ . '/bootstrap/app.php';
-
-$request = Illuminate\Http\Request::capture();
-$response = $app->handle($request);
-$response->send();
-
-$app->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
