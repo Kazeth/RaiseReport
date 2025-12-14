a807e61895a51e39f7521553a5fe3463b257cdec
Kazeth <roonysoon88@gmail.com>
2025-12-14 23:42:31 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => AdminMiddleware::class,
             'user'  => UserMiddleware::class,
             'proxy' => TrustProxies::class,
-            'locale' => SetLocale::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
