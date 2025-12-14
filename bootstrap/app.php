3b7236d56b589f5e4be8b9d2e1952942a0db212a
Kazeth <roonysoon88@gmail.com>
2025-12-14 23:40:12 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,10 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'proxy' => TrustProxies::class,
             'locale' => SetLocale::class,
         ]);
-
-        $middleware->appendToGroup('web', [
-            SetLocale::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
